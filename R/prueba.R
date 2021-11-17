25c5d4d7d5669b8c3f9bf49cbc3abd4b05c56a7b
hectorchavezgaro44 <hector.chaga94@gmail.com>
2021-11-17 10:08:22 -0600

    sube cambios en prueba.R con plantilla de actualizada

--- R/prueba.R
+++ R/prueba.R
@@ -15,15 +15,22 @@
 #
 #
 # # Leer cuestionario -------------------------------------------------------
-cuestionario <- officer::read_docx("~/Downloads/Morant Consultores plantilla.docx")
-docx_summary(cuestionario) %>%
-  filter(!style_name %in% c("Morant_título","Morant_texto")) %>%
+cuestionario <- officer::read_docx("~/Downloads/Encuesta_Plantilla_16Nov.docx")
+officer::docx_summary(cuestionario) %>%
+  filter(!is.na(style_name),style_name %in% c("Morant_Bloque","Morant_Pregunta",
+                            "Morant_respuetas")) %>%
+  select(-c(level:row_span)) %>%
   mutate(bloque=ifelse(style_name=="Morant_Bloque", text, NA)) %>%
   fill(bloque,.direction = c("down")) %>%
   filter(!style_name=="Morant_Bloque") %>%
   mutate(pregunta=ifelse(style_name=="Morant_Pregunta", text, NA)) %>%
   fill(pregunta,.direction = c("down")) %>%
-  filter(!style_name=="Morant_Pregunta")
+  filter(!style_name=="Morant_Pregunta") %>%
+  group_by(bloque, pregunta) %>%
+  summarise(respuestas=list(text)) %>%
+  ungroup() %>%
+  mutate(llaves=stringr::str_extract(pregunta, "\\{.+\\}")) %>%
+  View()
 #
 # # # Leer diccionario
 # # diccionario <- read_csv(file = "datar/codigos.csv")
